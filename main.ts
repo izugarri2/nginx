80a479790dafe699cd53e9b26cb4a8e269591e85
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2023-02-01 20:18:36 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,7 @@
-import { serve } from "https://deno.land/std@0.175.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.175.0/http/file_server.ts";
 
-serve((req) => {
-  const pathname = new URL(req.url).pathname;
-  if (pathname.startsWith("/static")) {
-    return serveDir(req, {
-      fsRoot: "/static",
-    });
-  }
-  // Do dynamic responses
-  return new Response();
+// ...
+serveDir(new Request("/static"), {
+  fsRoot: "public",
+  urlRoot: "static",
 });
