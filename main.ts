f9f01a82c84ac7a129b7d30bba83320d06a6dcab
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2023-02-01 20:09:56 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,77 +1,13 @@
-import { Server } from "https://deno.land/std@0.175.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.175.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.175.0/http/file_server.ts";
 
-import {
-  Application,
-  HttpError,
-  send,
-  Status
-} from "https://deno.land/x/oak/mod.ts";
-
-const app = new Application();
-
-// Error handler middleware
-app.use(async (context, next) => {
-  try {
-    await next();
-  } catch (e) {
-    if (e instanceof HttpError) {
-      context.response.status = e.status as any;
-      if (e.expose) {
-        context.response.body = `<!DOCTYPE html>
-        <html>
-            <body>
-            <h1>${e.status} - ${e.message}</h1>
-            </body>
-        </html>`;
-      } else {
-        context.response.body = `<!DOCTYPE html>
-        <html>
-            <body>
-            <h1>${e.status} - ${Status[e.status]}</h1>
-            </body>
-        </html>`;
-      }
-    } else if (e instanceof Error) {
-      context.response.status = 500;
-      context.response.body = `<!DOCTYPE html>
-        <html>
-            <body>
-            <h1>500 - Internal Server Error</h1>
-            </body>
-        </html>`;
-      console.log("Unhandled Error:", red(bold(e.message)));
-      console.log(e.stack);
-    }
+serve((req) => {
+  const pathname = new URL(req.url).pathname;
+  if (pathname.startsWith("/static")) {
+    return serveDir(req, {
+      fsRoot: "path/to/static/files/dir",
+    });
   }
+  // Do dynamic responses
+  return new Response();
 });
-
-// Logger
-app.use(async (context, next) => {
-  await next();
-  const rt = context.response.headers.get("X-Response-Time");
-  console.log(
-    `${green(context.request.method)} ${cyan(context.request.url)} - ${bold(
-      String(rt)
-    )}`
-  );
-});
-
-// Response Time
-app.use(async (context, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  context.response.headers.set("X-Response-Time", `${ms}ms`);
-});
-
-// Send static content
-app.use(async context => {
-  await send(context, context.request.path, {
-    root: `${Deno.cwd()}/static`,
-    index: "index.html"
-  });
-});
-
-const address = "127.0.0.1:8000";
-console.log(bold("Start listening on ") + yellow(address));
-await app.listen(address);
