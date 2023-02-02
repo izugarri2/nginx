fd39917c39a5a417bf964c419e4e6a79c4534e19
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2023-02-02 17:17:22 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,41 +2,6 @@ import { Application, HttpError, send, Status } from "https://deno.land/x/oak/mo
 
 const app = new Application();
 
-// Error handler middleware
-app.use(async (context, next) => {
-  try {
-    await next();
-  } catch (e) {
-    if (e instanceof HttpError) {
-      context.response.status = e.status as any;
-      if (e.expose) {
-        context.response.body = `<!DOCTYPE html>
-        <html>
-            <body>
-            <h1>${e.status} - ${e.message}</h1>
-            </body>
-        </html>`;
-      } else {
-        context.response.body = `<!DOCTYPE html>
-        <html>
-            <body>
-            <h1>${e.status} - ${Status[e.status]}</h1>
-            </body>
-        </html>`;
-      }
-    } else if (e instanceof Error) {
-      context.response.status = 500;
-      context.response.body = `<!DOCTYPE html>
-        <html>
-            <body>
-            <h1>500 - Internal Server Error</h1>
-            </body>
-        </html>`;
-      console.log("Unhandled Error:", (e.message));
-      console.log(e.stack);
-    }
-  }
-});
 
 // Send static content
 app.use(async (ctx) => {
