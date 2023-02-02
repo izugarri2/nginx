6c5db0e6d2a1e2fbc352446c440b480948e8524e
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2023-02-02 17:09:59 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,69 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, HttpError, send, Status } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
+// Error handler middleware
+app.use(async (context, next) => {
+  try {
+    await next();
+  } catch (e) {
+    if (e instanceof HttpError) {
+      context.response.status = e.status as any;
+      if (e.expose) {
+        context.response.body = `<!DOCTYPE html>
+        <html>
+            <body>
+            <h1>${e.status} - ${e.message}</h1>
+            </body>
+        </html>`;
+      } else {
+        context.response.body = `<!DOCTYPE html>
+        <html>
+            <body>
+            <h1>${e.status} - ${Status[e.status]}</h1>
+            </body>
+        </html>`;
+      }
+    } else if (e instanceof Error) {
+      context.response.status = 500;
+      context.response.body = `<!DOCTYPE html>
+        <html>
+            <body>
+            <h1>500 - Internal Server Error</h1>
+            </body>
+        </html>`;
+      console.log("Unhandled Error:", (e.message));
+      console.log(e.stack);
+    }
+  }
+});
+
+// Logger
+app.use(async (context, next) => {
+  await next();
+  const rt = context.response.headers.get("X-Response-Time");
+  console.log(
+    `${(context.request.method)} ${(context.request.url)} - ${(
+      String(rt)
+    )}`
+  );
+});
+
+// Response Time
+app.use(async (context, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  context.response.headers.set("X-Response-Time", `${ms}ms`);
+});
 
 // Send static content
 app.use(async context => {
   await send(context, context.request.path, {
-    root: `${Deno.cwd()}/static`,
+    root: `${Deno.cwd()}`,
     index: "index.html"
   });
 });
 
-await app.listen({ port: 8000 });
+const address = "127.0.0.1:8000";
+await app.listen(address);
