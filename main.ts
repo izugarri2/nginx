161a49646f9ae7d0e15d4d64dfc9a7ae9c8a5fe4
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2023-02-02 17:31:26 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,14 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
-app.use(async (ctx) => {
-  try {
-    await ctx.send({
-      root: `${Deno.cwd()}`,
-      index: "index.html",
-    });
-  } catch {
-    ctx.response.status = 404;
-    ctx.response.body = "404 - No found - Página no encontrada";
-  }
+
+
+// Send static content
+app.use(async => {
+  await {
+    root: `${Deno.cwd()}`,
+    index: "index.html"
+  };
 });
 
 await app.listen({ port: 8000 });
