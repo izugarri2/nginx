dbe27e124817675e4da6fdb2b67647e3bbecf31b
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2023-02-02 17:41:23 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
-
-
-// Send static content
 app.use(async => {
   try {
-    root: `${Deno.cwd()}`,
-    index: "index.html",
-  };
+    await {
+      root: `${Deno.cwd()}`,
+      index: "index.html",
+    };
+  } 
 });
 
 await app.listen({ port: 8000 });
